f7a206a8396c493023711a8cf469fd7d22941b9e
Hoang Thi Xa <137850069+gomeo02@users.noreply.github.com>
2023-08-29 08:26:16 +0700

    Create down.sh

---
+++ down.sh
@@ -0,0 +1,6 @@
+echo "Downloading LICENSE and Acceptable Usage Policy"
+wget ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
+wget ${PRESIGNED_URL/'*'/"USE_POLICY.md"} -O ${TARGET_FOLDER}"/USE_POLICY.md"
+
+for m in ${MODEL_SIZE//,/ }
+do
