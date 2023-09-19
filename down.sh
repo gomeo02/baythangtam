19e412e6db899771a0abf1763061aa659eb1dc95
Hoang Thi Xa <137850069+gomeo02@users.noreply.github.com>
2023-09-19 07:57:53 +0700

    Update down.sh

--- down.sh
+++ down.sh
@@ -4,5 +4,4 @@ wget ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
 wget ${PRESIGNED_URL/'*'/"USE_POLICY.md"} -O ${TARGET_FOLDER}"/USE_POLICY.md"
 
 for m in ${MODEL_SIZE//,/ }
-done
 }
